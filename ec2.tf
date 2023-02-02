063c4b7f0ffdff8ec7434a3a9a97bc53cff9dce1
pabitro <pabitro.ece@gmail.com>
2023-02-02 15:49:42 +0600

    main branch created

--- ec2.tf
+++
@@ -1,4 +0,0 @@
-resource "aws_instance" "web-server-linux1" {
-  ami           = "ami-05fa00d4c63e32376"
-  instance_type = "t2.micro"
-}
--- provider.tf
+++
@@ -1,5 +0,0 @@
-provider "aws" {
-  region     = "us-east-1"
-  access_key = "AKIAWIIGXQ5F4KCXAHYB"
-  secret_key = "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"
-}
