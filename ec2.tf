e470aebd87493cf176ab9c6538ea894dd926a5fb
pabitro56 <60471652+pabitro56@users.noreply.github.com>
2023-02-17 16:54:43 +0600

    comment

--- ec2.tf
+++ ec2.tf
@@ -1,7 +1,7 @@
-resource "aws_instance" "web-server-linux1" {
-  ami           = "ami-0c2b0d3fb02824d92"
-  instance_type = "t2.micro"
-}
+#resource "aws_instance" "web-server-linux1" {
+#  ami           = "ami-0c2b0d3fb02824d92"
+#  instance_type = "t2.micro"
+#}
 
 resource "aws_vpc" "main" {
   cidr_block       = "10.0.0.0/16"
