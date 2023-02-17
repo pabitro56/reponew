7d578e6a3f76a46c9912b4a21d97040ed3aa870a
pabitro56 <60471652+pabitro56@users.noreply.github.com>
2023-02-17 16:51:51 +0600

    vpc

--- ec2.tf
+++ ec2.tf
@@ -2,3 +2,12 @@ resource "aws_instance" "web-server-linux1" {
   ami           = "ami-0c2b0d3fb02824d92"
   instance_type = "t2.micro"
 }
+
+resource "aws_vpc" "main" {
+  cidr_block       = "10.0.0.0/16"
+  instance_tenancy = "default"
+
+  tags = {
+    Name = "main"
+  }
+}
