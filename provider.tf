df0834f97c6c0fb3711f95e0db3404a4f6d0b773
pabitro56 <60471652+pabitro56@users.noreply.github.com>
2023-02-20 10:26:27 +0600

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,5 +1,5 @@
 provider "aws" {
   region     = "us-east-1"
-  access_key = "AKIAWIIGXQ5FR62XXGSJ"
-  secret_key = "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"
+  access_key = ""
+  secret_key = ""
 }
